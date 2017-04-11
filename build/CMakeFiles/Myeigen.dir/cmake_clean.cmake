4c67b4f237553d36ff801ec9b2af8631a1223abe
abner <356337092.com>
2017-04-11 14:20:47 +0800

    first commit

---
+++ build/CMakeFiles/Myeigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Myeigen.dir/first.cpp.o"
+  "libMyeigen.pdb"
+  "libMyeigen.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Myeigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
